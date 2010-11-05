3be7cddaeb8275b618848d768adcb006cfa265df
frankverbeke <frankverbeke@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2010-11-05 11:20:32 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@52 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- web/util/updatestats.bat
+++ web/util/updatestats.bat
@@ -1,2 +1,2 @@
 java -Xms64m -Xmx384m -classpath ../WEB-INF/classes;../WEB-INF/lib/mysql-connector-java-5.1.10-bin.jar;../WEB-INF/lib/log4j-1.2.15.jar;../WEB-INF/lib/primrose.jar;../WEB-INF/lib/sqljdbc.jar;../WEB-INF/lib/dom4j-full.jar be.openclinic.statistics.UpdateStatsDb "c:/program files/apache software foundation/tomcat 6.0/conf/db.cfg"
-//exit
+exit
