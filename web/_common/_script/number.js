f52a74aae53d5aa700c5d3c469304a1f2e4c81d0
frankverbeke <frankverbeke@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2012-12-08 11:37:13 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@571 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- web/_common/_script/number.js
+++ web/_common/_script/number.js
@@ -1,8 +1,9 @@
 function format_number(p,d){
-  var r;
-  if(p<0){p=-p;r=format_number2(p,d);r="-"+r;}
-  else   {r=format_number2(p,d);}
-  return r;
+  return p.toFixed(d);
+  //var r;
+  //if(p<0){p=-p;r=format_number2(p,d);r="-"+r;}
+  //else   {r=format_number2(p,d);}
+  //return r;
 }
 
 function format_number2(pnumber,decimals){
