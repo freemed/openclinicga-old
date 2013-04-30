be2fe9e3961c6adc9701dbf87258cf8b479d76b3
frankverbeke <frankverbeke@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2013-04-30 16:28:32 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@632 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- web/util/updateDatacenterIpAddresses.sh
+++ web/util/updateDatacenterIpAddresses.sh
@@ -1,1 +1,2 @@
-java -classpath ../WEB-INF/classes:../WEB-INF/lib/dom4j-full.jar:../WEB-INF/lib/mail.jar:../WEB-INF/lib/log4j.jar:../WEB-INF/lib/mysql-connector-java-5.1.10-bin.jar be.mxs.common.util.io.UpdateDatacenterIPAddresses "user=root&password="
+cd /var/tomcat/webapps/globalhealthbarometer/util
+java -classpath ../WEB-INF/classes:../WEB-INF/lib/dom4j-full.jar:../WEB-INF/lib/commons-httpclient-3.1.jar:../WEB-INF/lib/commons-logging.jar:../WEB-INF/lib/commons-codec-1.2.jar:../WEB-INF/lib/mail.jar:../WEB-INF/lib/log4j.jar:../WEB-INF/lib/mysql-connector-java-5.1.10-bin.jar be.mxs.common.util.io.UpdateDatacenterIPAddresses "user=root&password="
