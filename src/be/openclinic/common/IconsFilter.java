a0881ae22036c9fbb41e141d9d4df55d8df8a03f
stijnsmets <stijnsmets@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2013-10-24 11:30:14 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@861 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

---
+++ src/be/openclinic/common/IconsFilter.java
@@ -0,0 +1,41 @@
+package be.openclinic.common;
+
+import be.mxs.common.util.db.MedwanQuery;
+import java.io.File;
+import java.io.FileFilter;
+
+public class IconsFilter implements FileFilter {
+    private String sDefaultExtensions = "ico,png,jpg,bmp,gif";
+    private String sExtensions = MedwanQuery.getInstance().getConfigString("iconExtensions",sDefaultExtensions);
+
+    //--- CONSTRUCTOR ---
+    public IconsFilter(){
+        super();
+
+        // check which extension are configed to be accepted
+        if(sExtensions.length()==0){
+            sExtensions = sDefaultExtensions;
+        }
+
+        sExtensions = sExtensions.toLowerCase();
+    }
+
+    //--- ACCEPT ----------------------------------------------------------------------------------
+    public boolean accept(File file) {
+        if(!file.isDirectory()){
+            String sFileName = file.getName().toLowerCase();
+            String sFileExtension = sFileName.substring(sFileName.lastIndexOf(".")+1);
+
+            return sExtensions.indexOf(sFileExtension) > -1;
+        }
+        else{
+            return false;
+        }
+    }
+
+    //--- GET DESCRIPTION -------------------------------------------------------------------------
+    public String getDescription() {
+        return "Any extension which is configured in 'iconExtensions'. ("+sExtensions+")";
+    }
+
+}
