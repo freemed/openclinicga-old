7840a75f543cf0ed234128d8608b65d3c98d95d2
stijnsmets <stijnsmets@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2014-02-17 14:23:27 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@967 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- web/_common/_script/buttons.js
+++ web/_common/_script/buttons.js
@@ -3,14 +3,19 @@ var sFormBeginStatus = myForm.innerHTML;
 var dropDownChecked = false;
 
 function setSaveButton(evt){
-
   if(window.myButton){
 	var clickedElement = getClickedElement(evt);
-    // Any input-element that needs to be saved has a name.
-    // Exclude buttons (to-do : except those that change an input-value) and the printlanguage-selector
-    if(clickedElement.type && clickedElement.type!="button" && (clickedElement.name!=null && clickedElement.name!="PrintLanguage")){
-      bSaveHasNotChanged = false;
-      dropDownChecked = false;
-    }
+	
+	try{
+	  // Any input-element that needs to be saved has a name. Exclude buttons
+	  if(clickedElement.type!=null && clickedElement.type!="button" && 
+	     clickedElement.name!=null && clickedElement.name!="PrintLanguage"){
+	    bSaveHasNotChanged = false;
+	    dropDownChecked = false;
+	  }
+	}
+	catch(err){
+	  /* 'attribute type does not exist' */	
+	}	
   }
 }
