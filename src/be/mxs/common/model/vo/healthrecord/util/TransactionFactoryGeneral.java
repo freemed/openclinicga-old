de72102a2b1549e11358b4d81ecda89beacf552c
stijnsmets <stijnsmets@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2014-07-31 09:30:28 +0000

    aanpassing zodat default-values van tranitems niet meer in bestaande onderzoekn terecht komen
    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@1279 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- src/be/mxs/common/model/vo/healthrecord/util/TransactionFactoryGeneral.java
+++ src/be/mxs/common/model/vo/healthrecord/util/TransactionFactoryGeneral.java
@@ -13,33 +13,42 @@ import java.util.Vector;
 
 public class TransactionFactoryGeneral extends TransactionFactory {
 
-    public TransactionVO createTransactionVO(UserVO userVO) {
-
+	//--- CREATE TRANSACTIONVO --------------------------------------------------------------------
+	// only for inheritance
+    public TransactionVO createTransactionVO(UserVO userVO){
         return null;
     }
-    public TransactionVO createTransactionVO(UserVO userVO,String transactionType) {
-        ItemContextVO itemContextVO = new ItemContextVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier()), "", "");
-        Vector items= MedwanQuery.getInstance().loadTransactionItems(transactionType,itemContextVO);
-        addItem(items, new ItemVO(     new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
-                                            IConstants.ITEM_TYPE_RECRUITMENT_CONVOCATION_ID,
-                                            this.getActiveContext().getConvocationId(),
-                                            new Date(),
-                                            itemContextVO));
-        addItem(items, new ItemVO(     new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
-                                            IConstants.ITEM_TYPE_CONTEXT_DEPARTMENT,
-                                            this.getActiveContext().getDepartment(),
-                                            new Date(),
-                                            itemContextVO));
-        addItem(items, new ItemVO(     new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
-                                            IConstants.ITEM_TYPE_CONTEXT_CONTEXT,
-                                            this.getActiveContext().getContext(),
-                                            new Date(),
-                                            itemContextVO));
-        TransactionVO transactionVO;
-        transactionVO = new TransactionVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier()),transactionType,new Date(),new Date(), IConstants.TRANSACTION_STATUS_CLOSED, userVO,items);
-        return transactionVO;
+    
+    //--- CREATE TRANSACTIONVO (2) ----------------------------------------------------------------
+    public TransactionVO createTransactionVO(UserVO userVO, String transactionType){
+        return createTransactionVO(userVO,transactionType,true);	
+    }
+    
+    public TransactionVO createTransactionVO(UserVO userVO, String transactionType, boolean loadDefaultValues){
+        ItemContextVO itemContextVO = new ItemContextVO(new Integer(IdentifierFactory.getInstance().getTemporaryNewIdentifier()),"","");
+        Vector items = MedwanQuery.getInstance().loadTransactionItems(transactionType,itemContextVO,loadDefaultValues);
+        
+        // add common items
+        addItem(items, new ItemVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
+                                    IConstants.ITEM_TYPE_RECRUITMENT_CONVOCATION_ID,
+                                    this.getActiveContext().getConvocationId(),
+                                    new Date(),
+                                    itemContextVO));
+        addItem(items, new ItemVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
+                                    IConstants.ITEM_TYPE_CONTEXT_DEPARTMENT,
+                                    this.getActiveContext().getDepartment(),
+                                    new Date(),
+                                    itemContextVO));
+        addItem(items, new ItemVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier() ),
+                                    IConstants.ITEM_TYPE_CONTEXT_CONTEXT,
+                                    this.getActiveContext().getContext(),
+                                    new Date(),
+                                    itemContextVO));
+        
+        return new TransactionVO(new Integer( IdentifierFactory.getInstance().getTemporaryNewIdentifier()),transactionType,new Date(),new Date(), IConstants.TRANSACTION_STATUS_CLOSED, userVO,items);
     }
 
+    //--- ADD ITEM --------------------------------------------------------------------------------
     private void addItem(Vector items,ItemVO item){
         for (int n=0;n<items.size();n++){
             if (((ItemVO)items.elementAt(n)).getType().equalsIgnoreCase(item.getType())){
@@ -48,4 +57,5 @@ public class TransactionFactoryGeneral extends TransactionFactory {
         }
         items.add(item);
     }
+    
 }
