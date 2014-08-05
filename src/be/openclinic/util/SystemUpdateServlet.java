abd579902d2fb2a20d5661caf743b2674de9e775
stijnsmets <stijnsmets@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2014-08-05 12:06:58 +0000

    unused import removed
    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@1297 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- src/be/openclinic/util/SystemUpdateServlet.java
+++ src/be/openclinic/util/SystemUpdateServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import be.mxs.common.util.system.ScreenHelper;
 import be.mxs.common.util.system.UpdateSystem;
 
 public class SystemUpdateServlet extends HttpServlet {
