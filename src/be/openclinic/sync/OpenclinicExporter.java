017dbdac2d076acd5cfb3b7d95b8847680d5ac73
frankverbeke <frankverbeke@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2014-08-16 13:14:13 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@1341 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

--- src/be/openclinic/sync/OpenclinicExporter.java
+++ src/be/openclinic/sync/OpenclinicExporter.java
@@ -10,6 +10,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
@@ -18,43 +23,105 @@ import org.dom4j.Element;
 import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
 
 import be.mxs.common.util.db.MedwanQuery;
+import be.mxs.common.util.system.HTMLEntities;
 import be.mxs.common.util.system.ScreenHelper;
 
 public class OpenclinicExporter {
+	public SortedMap patientrecordblocks = new TreeMap();
 	public Hashtable patientrecords = new Hashtable();
 	public Date begin,end;
+	public int maxrecordblocks=MedwanQuery.getInstance().getConfigInt("exportMaxRecordBlocks",10000);
 	
 	public String run(){
 		begin = getLastExport();
-		end = getLastExport();;
 		addAdmin();
-
+		addPrivate();
+		//Now we move the recordblocks to patientrecords
+		Iterator i = patientrecordblocks.keySet().iterator();
+		while(i.hasNext()){
+			String key = (String)i.next();
+			addToRecord(Integer.parseInt(key.split("\\.")[1]), (Element)patientrecordblocks.get(key));
+		}
 		Document document = DocumentHelper.createDocument();
 		Element export = document.addElement("export");
 		export.addAttribute("begin", new SimpleDateFormat("yyyyMMddHHmmssSSSS").format(begin));
-		export.addAttribute("end", new SimpleDateFormat("yyyyMMddHHmmssSSSS").format(end));
 		Enumeration e = patientrecords.keys();
 		while(e.hasMoreElements()){
 			int personid = (Integer)e.nextElement();
 			export.add((Element)patientrecords.get(personid));
 		}
-		//setLastExport(end);
+		/*
+		if(patientrecordblocks.size()>0){
+			try {
+				setLastExport(new SimpleDateFormat("yyyyMMddHHmmssSSSS").parse(((String)patientrecordblocks.lastKey()).split("\\.")[0]));
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+		}
+		*/
 		return export.asXML();
+	}
+	
+	public void addPrivate(){
+		Connection conn = MedwanQuery.getInstance().getAdminConnection();
+		try {
+			PreparedStatement ps = conn.prepareStatement("select * from adminprivate where updatetime>=? order by updatetime asc");
+			ps.setTimestamp(1, new java.sql.Timestamp(begin.getTime()));
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				int personid= rs.getInt("personid");
+		        Timestamp updatetime = rs.getTimestamp("updatetime");
+		        Element admin = DocumentHelper.createElement("adminprivate");
+		        addTimestampElement(admin, "updatetime", updatetime);
+		        addDateElement(admin, "start", rs.getDate("start"));
+		        addDateElement(admin, "stop", rs.getDate("stop"));
+		        addStringElement(admin, "address", rs.getString("address"));
+		        addStringElement(admin, "city", rs.getString("city"));
+		        addStringElement(admin, "zipcode", rs.getString("zipcode"));
+		        addStringElement(admin, "country", rs.getString("country"));
+		        addStringElement(admin, "telephone", rs.getString("telephone"));
+		        addStringElement(admin, "fax", rs.getString("fax"));
+		        addStringElement(admin, "mobile", rs.getString("mobile"));
+		        addStringElement(admin, "email", rs.getString("email"));
+		        addStringElement(admin, "comment", rs.getString("comment"));
+		        addStringElement(admin, "type", rs.getString("type"));
+		        addStringElement(admin, "updateserverid", rs.getString("updateserverid"));
+		        addStringElement(admin, "district", rs.getString("district"));
+		        addStringElement(admin, "sanitarydistrict", rs.getString("sanitarydistrict"));
+		        addStringElement(admin, "province", rs.getString("province"));
+		        addStringElement(admin, "sector", rs.getString("sector"));
+		        addDateElement(admin, "cell", rs.getDate("cell"));
+		        addDateElement(admin, "quarter", rs.getDate("quarter"));
+		        addStringElement(admin, "business", rs.getString("business"));
+		        addStringElement(admin, "businessfunction", rs.getString("businessfunction"));
+		        if(!addRecordBlock(new SimpleDateFormat("yyyyMMddHHmmssSSS").format(updatetime)+"."+personid+".P", admin)){
+		        	break;
+		        }
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void addAdmin(){
 		Connection conn = MedwanQuery.getInstance().getAdminConnection();
 		try {
-			PreparedStatement ps = conn.prepareStatement("select * from admin where updatetime>=? order by updatetime desc");
+			PreparedStatement ps = conn.prepareStatement("select * from admin where updatetime>=? order by updatetime asc");
 			ps.setTimestamp(1, new java.sql.Timestamp(begin.getTime()));
 			ResultSet rs = ps.executeQuery();
-			while(rs.next() && patientrecords.size()<1000){
-				//Create an admin block and save it to the patient record
-		        Element admin = DocumentHelper.createElement("admin");
+			while(rs.next()){
+				int personid= rs.getInt("personid");
 		        Timestamp updatetime = rs.getTimestamp("updatetime");
-		        if(updatetime.after(end)){
-		        	end=updatetime;
-		        }
+		        Element admin = DocumentHelper.createElement("admin");
 		        addStringElement(admin, "immatold", rs.getString("immatold"));
 		        addStringElement(admin, "immatnew", rs.getString("immatnew"));
 		        addStringElement(admin, "candidate", rs.getString("candidate"));
@@ -93,7 +160,9 @@ public class OpenclinicExporter {
 		        addDateElement(admin, "begindate", rs.getDate("begindate"));
 		        addDateElement(admin, "enddate", rs.getDate("enddate"));
 		        addStringElement(admin, "archivefilecode", rs.getString("archivefilecode"));
-		        addToRecord(rs.getInt("personid"), admin);
+		        if(!addRecordBlock(new SimpleDateFormat("yyyyMMddHHmmssSSS").format(updatetime)+"."+personid+".A", admin)){
+		        	break;
+		        }
 			}
 			rs.close();
 			ps.close();
@@ -128,6 +197,23 @@ public class OpenclinicExporter {
 	public OpenclinicExporter(){
 		
 	}
+	
+	private boolean addRecordBlock(String key, Element element){
+		if(patientrecordblocks.size()>maxrecordblocks){
+			try {
+				if(new SimpleDateFormat("yyyyMMddHHmmssSSS").parse(((String)patientrecordblocks.lastKey()).split("\\.")[0]).after(new SimpleDateFormat("yyyyMMddHHmmssSSS").parse(key.split("\\.")[0]))){
+					patientrecordblocks.remove(patientrecordblocks.lastKey());
+				}
+				else{
+					return false;
+				}
+			} catch (ParseException e) {
+				return false;
+			}
+		}
+		patientrecordblocks.put(key, element);
+		return true;
+	}
 
 	private void addToRecord(int personid, Element element){
 		if(patientrecords.get(personid)==null){
@@ -142,7 +228,7 @@ public class OpenclinicExporter {
 	private void addStringElement(Element parent, String elementName, String elementValue){
 		if(elementValue!=null && elementValue.length()>0){
 			Element e = parent.addElement(elementName);
-			e.setText(elementValue);
+			e.setText(HTMLEntities.htmlentities(elementValue));
 			
 		}
 	}
@@ -160,5 +246,6 @@ public class OpenclinicExporter {
 			e.setText(new SimpleDateFormat("yyyyDDmmHHmmssSSS").format(elementValue));
 		}
 	}
+	
 	
 }
