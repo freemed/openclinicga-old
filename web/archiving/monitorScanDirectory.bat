167305f62eaa05e45cf704e4f71dfd849847118e
stijnsmets <stijnsmets@fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556>
2014-08-25 14:20:26 +0000

    git-svn-id: http://svn.code.sf.net/p/open-clinic/code@1373 fff6d2c3-ad4f-46bf-9e3e-7ed8f9d13556

---
+++ web/archiving/monitorScanDirectory.bat
@@ -0,0 +1,1 @@
+java -classpath ../WEB-INF/classes/;../WEB-INF/lib/log4j.jar;../WEB-INF/lib/jtds-0.9.jar;../WEB-INF/lib/mysql-connector-java-5.1.10-bin.jar;../WEB-INF/lib/CacheDB.jar;../WEB-INF/lib/dom4j-full.jar;../WEB-INF/lib/mail.jar;../WEB-INF/lib/activation.jar be.openclinic.archiving.ScanDirectoryMonitorMain
